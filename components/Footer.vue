70b1bc7e2219067e8413aae7a35906d0959915f8
Hector del Angel <hectordelangel@gmail.com>
2021-03-03 10:20:33 -0800

    add simple footer

---
+++ components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <footer class="footer d-flex pt-2 flex-row">
+      <div class="align-items-center">
+        <p class="ml-1">
+          Simple site build w/Nuxt and Bootstrap by <a href="https://github.com/h3c70rdelan9el">h3c70r</a>
+        </p>
+      </div>
+      <div class="ml-auto mr-2">
+        <span>Images by</span>
+        <a href="https://www.unspash.com">Unsplash</a>
+        <span>Logo by</span>
+        <a href="https://www.logomakr.com">logomakr</a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<style>
+  .footer {
+    background-color: black;
+    color: white;
+    padding: 2px;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="">
     <Carousel class="carousel" />
+    <Footer />
   </div>
 </template>
 
